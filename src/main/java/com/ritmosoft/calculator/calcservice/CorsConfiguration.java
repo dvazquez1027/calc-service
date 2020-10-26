6edc7ac5f1c65a67a630e5cc690f7e08fd50fa0a
David Vazquez <david.vazquez@vetsez.com>
2020-10-26 08:14:18 -0500

    Allow DELETE

--- src/main/java/com/ritmosoft/calculator/calcservice/CorsConfiguration.java
+++ src/main/java/com/ritmosoft/calculator/calcservice/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE");
             }
         };
     }
